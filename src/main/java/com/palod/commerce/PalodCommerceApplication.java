0ad6a54c3655a41ca3ba3af20b01062cd260eab1
Fatih ŞİMŞEK <efatih0@gmail.com>
2020-07-11 11:50:50 +0300

    created spring-boot app

---
+++ src/main/java/com/palod/commerce/PalodCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.palod.commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalodCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalodCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/palod/commerce/PalodCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palod.commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalodCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
